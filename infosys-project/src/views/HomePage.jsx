331c667bb17d957114365f153555221454171480
Leonardo Ringo <leonardoringo12@gmail.com>
2023-12-06 17:40:25 +0700

    feat: Done creating Services Cards

--- infosys-project/src/views/HomePage.jsx
+++ infosys-project/src/views/HomePage.jsx
@@ -9,13 +9,20 @@ import GroupeBrand from "../assets/Groupe.jpg";
 import ESPRITBrand from "../assets/ESPRIT.jpg";
 import ChanelBrand from "../assets/CHANEL.jpg";
 import BurberryBrand from "../assets/BURBERRY.jpg";
+import BackgroundDiagram from "../assets/Background Diagram.jpg";
+import FrontIcon from "../assets/Icon Front.jpg";
+import SEOService from "../assets/SEOService.jpg";
+import SMOService from "../assets/SMOService.jpg";
+import UXService from "../assets/UXService.jpg";
+import CMService from "../assets/CMService.jpg";
+import AdsService from "../assets/AdsService.jpg";
 
 export default function HomePage() {
   return (
     <>
       {/* ====================DIGITAL MARKETING SECTION==================== */}
       <div className="h-[533px] w-[1493.78px] top-[197px] left-[63px] flex justify-evenly">
-        <div className="">
+        <div>
           <div className="h-[25%] border-r border-gray-300 mx-4 mb-10"></div>
           <div className=" text-center mb-3 mt-3 space-y-5">
             <p className="text-[#9A9EA8]">YT</p>
@@ -37,7 +44,7 @@ export default function HomePage() {
               AGENCY
             </h1>
           </div>
-          <div className=" max-w-[47%] mt-3">
+          <div className=" text-[#8B919E] w-[500px] max-w-[60%] mt-3">
             <p>
               We examine the top of funnel to figure out how to make people to a
               subscribe newsletter then turn prospects into leads.
@@ -85,14 +92,76 @@ export default function HomePage() {
       </div>
 
       {/* ====================BRAND LOGO SECTION====================*/}
-      <div className="flex mt-36 gap-10 h-10">
+      <div className="flex mt-36 gap-10 h-10 mb-[10%]">
         <img src={GroupeBrand} alt="" />
         <img src={ESPRITBrand} alt="" />
         <img src={BurberryBrand} alt="" />
         <img src={ChanelBrand} alt="" />
       </div>
-      <div>Improve Performance</div>
-      <div>Our Services</div>
+
+      {/* ====================SEARCH ENGINE OPTIMIZATION SECTION====================*/}
+      <div className="w-[990px] h-[430.95px] top-[1090px] left-[272px] mb-[10%] flex">
+        <div className="w-[50%]">
+          {/* <img
+            src={BackgroundDiagram}
+            alt=""
+            className="object-cover object-center absolute inset-0"
+          /> */}
+          <img src={FrontIcon} alt="" className="w-[184px] h-[430.95px]" />
+        </div>
+        <div className="w-[50%]">
+          <p className="my-5 text-[#8B919E]">Search Engine Optimization</p>
+          <h1 className=" text-[40px] font-bold text-[#3F4352] max-w-[95%]">
+            Improve Performance and Growth your Website
+          </h1>
+          <p className="my-5 text-[#8B919E] max-w-md">
+            It's a theory of psychology that prioritizes the most fundamental
+            human needs (​like air, water, and physical safety) over more
+            advanced needs.
+          </p>
+          <button className="text-[#EE4E39] text-[16px] font-bold hover:underline">
+            Explore Our SEO’s Strategies {">"}
+          </button>
+        </div>
+      </div>
+
+      {/* ====================OUR SERVICES SECTION====================*/}
+      <div className="w-[888px] h-[671px] flex flex-col items-center">
+        <h1 className="text-[#3F4352] text-[24px]">Our Services</h1>
+        <p className="text-center max-w-[50%] my-5 text-[#8B919E]">
+          Increase your linkability, make tagging and bookmarking easy. Reward
+          inbound links
+        </p>
+        <div className="w-[888px] h-[191px] mt-10 flex gap-5 ">
+          <div className="w-[280px] h-[180px] flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
+            <img src={SEOService} alt="" className="w-12 h-12 mb-5" />
+            <p className="text-[16px] text-[#3F4352]">
+              Search Engine Optimization
+            </p>
+          </div>
+          <div className="w-[280px] h-[180px] flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
+            <img src={SMOService} alt="" className="w-12 h-12 mb-5" />
+            <p className="text-[16px] text-[#3F4352]">
+              Social Media Optimization
+            </p>
+          </div>
+          <div className="w-[280px] h-[180px] flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
+            <img src={UXService} alt="" className="w-12 h-12 mb-5" />
+            <p className="text-[16px] text-[#3F4352]">UX Writer</p>
+          </div>
+        </div>
+        <div className="w-[584px] h-[180px] flex gap-6">
+          <div className="w-[280px] h-[180px] flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
+            <img src={CMService} alt="" />
+            <p className="text-[16px] text-[#3F4352]">Content Management</p>
+          </div>
+          <div className="w-[280px] h-[180px] flex flex-col items-center hover:opacity-70 hover:cursor-pointer">
+            <img src={AdsService} alt="" />
+            <p className="text-[16px] text-[#3F4352]">Ads Optimization</p>
+          </div>
+        </div>
+      </div>
+
       <div>Our Teams</div>
     </>
   );
